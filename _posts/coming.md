b56827e4484252efc9346af991988ded7ba72313
David A. Viramontes <dviramontes@gmail.com>
2020-05-17 19:49:37 -0600

    chore: delete dummy posts, update copy

--- _posts/coming.md
+++ _posts/coming.md
@@ -1,13 +1,13 @@
 ---
-title: 'Learn How to Pre-render Pages Using Static Generation with Next.js'
-excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus.'
-coverImage: '/assets/blog/hello-world/cover.jpg'
-date: '2020-03-16T05:35:07.322Z'
+title: 'Our new site is alsmost here!'
+excerpt: 'visit us again soon! ^__^'
+coverImage: '/assets/blog/coming/cover.jpg'
+date: '2020-05-16T05:35:07.322Z'
 author:
   name: David Viramontes
   picture: '/assets/blog/authors/dav.gif'
 ogImage:
-  url: '/assets/blog/hello-world/cover.jpg'
+  url: '/assets/blog/coming/cover.jpg'
 ---
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Praesent elementum facilisis leo vel fringilla. Congue mauris rhoncus aenean vel. Egestas sed tempus urna et pharetra pharetra massa massa ultricies.
--- components/intro.js
+++ components/intro.js
@@ -2,38 +2,37 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1
-        style={{
-          transform: "rotate(45deg)",
-        }}
+        style={{ transform: "rotate(45deg)" }}
         className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8"
       >
         +
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A blog by{" "}
+        A blog and project showcase by{" "}
         <a
-          href="https://nextjs.org/"
+          href="https://www.linkedin.com/in/eemshi/"
           className="underline hover:text-success duration-200 transition-colors"
         >
           Michelle Lim
         </a>{" "}
         and{" "}
         <a
-          href="https://nextjs.org/"
+          href="https://www.linkedin.com/in/davidviramontes/"
           className="underline hover:text-success duration-200 transition-colors"
         >
           David Viramontes
         </a>{" "}
         <br />{" "}
-        <span style={{ background: "black", padding: '1px 2px', color: 'white' }}>
-	        $ go checkout our work @
+        <span style={{ background: "black", padding: '1px', color: 'white' }}>
+	        $ go checkout our past projects @
         <a
+	      id="mmmanyfolddotcom"
           href="http://mmmanyfold.com"
-          className="underline hover:text-success duration-200 transition-colors"
+          className="underline duration-200 transition-colors"
         >
-          mmmanyfold.com
+          {" "}mmmanyfold.com
         </a>{" "}
-	        <span className="blinking-cursor">|</span>{" "}
+	        <span className="blinking-cursor">|</span>
         </span>
       </h4>
     </section>
