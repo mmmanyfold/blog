dc3357c29858f45f9397da1965319876e83d632d
dviramontes <dviramontes@gmail.com>
2021-01-24 22:29:26 -0700

    get items from contentful api

--- components/layout.js
+++ components/layout.js
@@ -1,16 +1,14 @@
-import Alert from "../components/alert";
 import Footer from "../components/footer";
 import Meta from "../components/meta";
 import Projects from "./projects";
 import Head from "./head";
 
-export default function Layout({ preview, children }) {
+export default function Layout({ children }) {
   return (
     <>
       <Meta />
       <Head />
       <div className="h-96 min-h-0">
-        <Alert preview={preview} />
         <main>{children}</main>
       </div>
       <Projects />
--- components/projects.js
+++ components/projects.js
@@ -1,32 +1,39 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { CDN_ASSETS, CDN_ENTRIES } from "../lib/constants";
+import Project from "./project";
 import Container from "./container";
 
 export default function Projects() {
+  const [assets, setAssets] = useState([]);
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    const getProjects = async () => {
+      try {
+        const {
+          data: { items: entryData },
+        } = await axios(CDN_ENTRIES);
+        const {
+          data: { items: assetData },
+        } = await axios(CDN_ASSETS);
+        setEntries(entryData);
+        setAssets(assetData);
+      } catch (e) {
+        throw e;
+      }
+    };
+
+    getProjects();
+  }, []);
+
   return (
     <Container>
       <a href="#projects">
         <h3 className="mb-4 text-3xl md:text-3xl font-bold tracking-tighter leading-tight">Projects</h3>
       </a>
-      <figure className="md:flex bg-gray-100 rounded-xl p-8 md:p-0">
-        <img
-          className="w-32 h-32 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-          src="/assets/tmp.jpg"
-          alt=""
-        />
-        <div className="" alt="" width="384" height="512">
-          <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-            <blockquote>
-              <p className="text-lg font-semibold">
-                “Tailwind CSS is the only framework that I've seen scale on large teams. It’s easy to customize, adapts
-                to any design, and the build size is tiny.”
-              </p>
-            </blockquote>
-            <figcaption className="font-medium">
-              <div className="text-cyan-600">Sarah Dayan</div>
-              <div className="text-gray-500">Staff Engineer, Algolia</div>
-            </figcaption>
-          </div>
-        </div>
-      </figure>
+    { entries.length > 0 && entries.map((entry, i) =>
+          <Project key={i} entry={entry} />)}
     </Container>
   );
 }
