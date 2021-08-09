9c69e780af3ff43399bdc03e99ff4f9a67383576
Michelle Lim <elle@mmmanyfold.com>
2021-08-08 20:26:51 -0600

    update intro

--- components/intro.js
+++ components/intro.js
@@ -1,9 +1,9 @@
 export default function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col md:flex-row flex md:items-center md:justify-between mt-8 md:mt-16 md:mb-12">
       <h2 className="mb-8 text-6xl md:text-8xl font-bold tracking-tighter leading-tight">Hello!</h2>
-      <h2 className="mb-8 ml-8 text-2xl md:text-3xl tracking-tighter leading-tight">
-        Welcome to a blog and project showcase by{" "}
+      <h2 className="mb-8 text-1xl md:text-right tracking-tighter leading-tight">
+        mmmanyfold is a creative dev studio by<br/>
         <a
           href="https://www.linkedin.com/in/eemshi/"
           className="underline hover:text-success duration-200 transition-colors"
